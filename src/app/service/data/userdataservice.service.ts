f8c7f4052a8785f39bb089fc8590d980988f740b
Ramkarlapudi <Ramkarlapudi1995@gmail.com>
2021-01-26 17:05:51 +0530

    Added UserProfile to update Data

--- src/app/service/data/userdataservice.service.ts
+++ src/app/service/data/userdataservice.service.ts
@@ -1,6 +1,8 @@
   import { HttpClient } from '@angular/common/http';
   import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
 import { UserProfile } from './usermodel';
+import { catchError } from 'rxjs/operators';
 
 
   
@@ -11,6 +13,9 @@ import { UserProfile } from './usermodel';
   })
   export class UserdataserviceService {
 
+    errrorArrived :boolean = false;
+    
+    
     constructor(private  http:HttpClient) { }
 
     executeGetUserService(){
@@ -19,11 +24,48 @@ import { UserProfile } from './usermodel';
 
     }
 
-    executeGetUserServicebyName(username){
+    executeGetUserServicebyName(username: any){
       console.log("executeGetUserService ");
            return  this.http.get<UserProfile>(`http://localhost:2021/userservice/getusersbyname/${username}`);
 
     }
+
+    
+
+
+    UpdateUserProfile(userprofile:UserProfile): Observable<UserProfile> {
+      const headers = { 'content-type': 'application/json'}  
+      const body=JSON.stringify(userprofile);
+      console.log(body)
+      return this.http.post<UserProfile>('http://localhost:2021/userservice/uploadProfile/' , body,{'headers':headers})
+      .pipe(
+
+        catchError(err => {
+          console.log('caught mapping error and rethrowing', err);
+          
+          return throwError(this.handleError(err));
+          
+      })
+      )
+    }
+
+
+    handleError(error: Response) {
+     // console.log("error.status " +error.status)
+      if (error.status === 0) {   
+       
+        this.errrorArrived = true;
+        
+      }else{
+        this.errrorArrived = false;
+        
+      }   
+    }
+        
+      // } else {
+      //   return Observable.throw(error);
+      // }
+ 
 
 
   }
--- src/app/userprofile/userprofile.component.ts
+++ src/app/userprofile/userprofile.component.ts
@@ -1,68 +1,144 @@
-  import { Component, OnInit } from '@angular/core';
-  import { UserdataserviceService } from '../service/data/userdataservice.service';
-  import { AuthenticationService } from '../service/authentication.service';
-  import { UserProfile } from '../service/data/usermodel';
+import { Component, OnInit } from '@angular/core';
+import { UserdataserviceService } from '../service/data/userdataservice.service';
+import { AuthenticationService } from '../service/authentication.service';
+import { UserProfile } from '../service/data/usermodel';
+import { Observable } from 'rxjs';
 
-  @Component({
-    selector: 'app-userprofile',
-    templateUrl: './userprofile.component.html',
-    styleUrls: ['./userprofile.component.css']
-  })
-  export class UserprofileComponent implements OnInit {
 
-    userprofiledata: UserProfile
-    isDataAvailable: boolean = false;
-    constructor(private userdataserviceService: UserdataserviceService, private authenticationService: AuthenticationService) { }
+@Component({
+  selector: 'app-userprofile',
+  templateUrl: './userprofile.component.html',
+  styleUrls: ['./userprofile.component.css']
+})
+export class UserprofileComponent implements OnInit {
 
-    ngOnInit(): void {
-      this.displayUserData();
+  userprofiledata: UserProfile
+  RefUserProfiledata: UserProfile
+  isDataAvailable: boolean = false;
+  verifiedUser: String = "";
+  UserID: any = "";
+  FirstName: String = "";
+  LastName: String = "";
+  UserName: String = "";
+  Email: String = "";
+  Phone: any = "";
+  Country: String = "";
+  State: String = "";
+  Address: String = "";
+  PinCode: any = "";
+  Verified: String = "";
+  errormessage ="Something Wrong : Profile Not Updated";
+  successMsg ="Your Profile Updated Successfully";
+  errorflag = false;
+  successflag = false;
+
+  constructor(private userdataserviceService: UserdataserviceService, private authenticationService: AuthenticationService) { }
+
+  ngOnInit(): void {
+    this.displayUserData();
+    this.UserID = this.userprofiledata.userid;
+    this.FirstName = this.userprofiledata.firstname;
+    this.LastName = this.userprofiledata.lastname;
+    this.UserName = this.userprofiledata.username;
+    this.Email = this.userprofiledata.email;
+    this.Phone = this.userprofiledata.phone;
+    this.Country = this.userprofiledata.country;
+    this.State = this.userprofiledata.state;
+    this.Address = this.userprofiledata.address;
+    this.PinCode = this.userprofiledata.pincode;
+    this.Verified = this.userprofiledata.verified;
+  }
+
+
+  displayUserData(): boolean {
+    console.log("Inside method");
+    var retrievedObject = sessionStorage.getItem('userpp');
+    this.userprofiledata = JSON.parse(retrievedObject);
+    if (retrievedObject != null) {
+
+
+      this.isDataAvailable = true;
+      return true;
     }
+    this.userprofiledata = this.authenticationService.userprofile;
+    this.verifiedUser = this.userprofiledata.verified;
+    sessionStorage.setItem("userpp", JSON.stringify(this.userprofiledata));
+    console.log(this.userprofiledata.userid);
+    console.log(this.userprofiledata.username);
+    if (this.userprofiledata != null) {
+      console.log("Inside if usercomp");
+      this.isDataAvailable = true;
 
-
-    displayUserData() :boolean{
-      console.log("Inside method");
+    } else {
       var retrievedObject = sessionStorage.getItem('userpp');
       this.userprofiledata = JSON.parse(retrievedObject);
-      if(retrievedObject != null){
-        
-        
-        this.isDataAvailable = true;
-        return true;
+      console.log("Inside else 1 usercomp");
+      if (this.userprofiledata === null) {
+        console.log("Inside else 2 usercomp");
+        this.isDataAvailable = false;
+
       }
-      this.userprofiledata = this.authenticationService.userprofile;
-      sessionStorage.setItem("userpp", JSON.stringify(this.userprofiledata));
-      console.log(this.userprofiledata.userid);
-      console.log(this.userprofiledata.username);
-      if (this.userprofiledata != null) {
-        console.log("Inside if usercomp");
-        this.isDataAvailable = true;
-        
-      } else {
-        var retrievedObject = sessionStorage.getItem('userpp');
-        this.userprofiledata = JSON.parse(retrievedObject);
-        console.log("Inside else 1 usercomp");
-        if(this.userprofiledata === null){
-          console.log("Inside else 2 usercomp");
-          this.isDataAvailable = false;
-        
-        }
-      }
-      return false;
+    }
+    return false;
+  }
+
+  refreshUserData() {
+    if (this.userprofiledata != null) {
+      console.log("Inside refreshUserData 1");
+      var retrievedObject = sessionStorage.getItem('userpp');
+      this.userprofiledata = JSON.parse(retrievedObject);
+    }
+    if (this.userprofiledata != null) {
+      console.log("Inside refreshUserData 2");
+      this.isDataAvailable = true;
     }
 
-    refreshUserData() {
-      if(this.userprofiledata === null){
-        console.log("Inside refreshUserData 1");
-        var retrievedObject = sessionStorage.getItem('userpp');
-        this.userprofiledata = JSON.parse(retrievedObject);
-      }
-      if (this.userprofiledata != null) {
-        console.log("Inside refreshUserData 2");
-        this.isDataAvailable = true;
-      }
-      
+  }
+
+  isVerifiedUser(): boolean {
+
+    if (this.verifiedUser === "YES") {
+      return true;
+    }
+
+    return false;
+  }
+
+
+  UpdateUserInfo() {
+
+  }
+
+
+  onContentChanged(event: Event) {
+    var textContent: any = event.target;
+    //let firstname : string = event.target.textContent;
+    this.userprofiledata.firstname = textContent;
+    // sessionStorage.setItem("userpp", JSON.stringify(this.userprofiledata.firstname));
+    console.log("updated user name " + this.userprofiledata.firstname)
+    // this.refreshUserData();
+
+  }
+
+
+  onSubmitValidate() {
+    //this.errorflag = false;
+    //this.successflag = false;
+    //  this.RefUserProfiledata.firstname = this.FirstName;
+    this.userdataserviceService.UpdateUserProfile(this.userprofiledata).subscribe(
+      data => this.userprofiledata = data
+    );
+    if(this.userdataserviceService.errrorArrived){
+        this.errorflag = true;
+    }else{
+      this.successflag = true;
     }
 
 
 
   }
+
+
+
+
+}
